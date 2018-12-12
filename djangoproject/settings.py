6ba905cf465e0e72794cac47dab8576552404729
Allelujaa <glassboy0123@gmail.com>
2018-12-12 12:36:45 +0900

    emergency added

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # Update the secret key to a value of your own before deploying the app.
-SECRET_KEY = 'lldtg$9(wi49j_hpv8nnqlh!cj7kmbwq0$rj7vy(b(b30vlyzj'
+SECRET_KEY = '***REMOVED***'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if os.environ.get('CHECK_INSTANCE'):
